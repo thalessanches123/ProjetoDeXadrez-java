7d0127b77ac91293ce9df224e46de0f7c9fe28a6
thalessanches123 <thales.sanches174@gmail.com>
2021-06-17 16:20:05 -0300

    chess layer and printing the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,27 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
-		
-		
-		
-		System.out.println("hello word");
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
 	}
 
 }
